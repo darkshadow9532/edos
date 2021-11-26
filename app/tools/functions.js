8245dee0f908ec69e1078086c853f7d89be86f23
root <root@localhost.localdomain>
2021-11-26 03:55:33 -0500

    Developing #2

--- app/tools/functions.js
+++ app/tools/functions.js
@@ -1,67 +1,121 @@
 // F1 - createCompareListFromInputAndName
-const NumLists = require("../controllers/numList.controller.js");
-const CompareLists = require("../controllers/compareList.controller.js");
+// const NumLists = require("../controllers/numList.controller.js");
+// const CompareLists = require("../controllers/compareList.controller.js");
 
 const db = require("../models");
 const NumList = db.numLists;
+const CompareList = db.compareLists
 
-class Model{
-    constructor(){
-        this.numLists = [];
-        this.compareList = {};
-    }
-
-    getNumLists(){
-        var that = this;
-        
-        NumLists.findAll(req, res)
-        .then(res => {
-            console.log(res);
-            that.numLists = res;
-        });
-    }
-
-    createCompareList(){
-        var compareList = new CompareLists({});
-        
-    }
-
-    compareNumListsWithArray(arr){
-        var that = this;
-        that.numLists.forEach(e => {
-            var value = countMatchingElements(e.numList, arr);
-            var temp =  {
-                name: e.name,
-                value: value
-            }
-            that.compareList.data.push(temp);
-        })
-    }
+function getLastStep(compareLists){
+    var maxStep = 0;
+    var lastStep = {};
+    compareLists.forEach(e =>{
+        if(e.phaseStep > maxStep){
+            maxStep = phaseStep;
+        }
+    })
+    compareLists.forEach(e =>{
+        if(e.phaseStep == maxStep){
+            lastStep = e;
+        }
+    })
+    return lastStep;
 }
 
-
-
-
-function countMatchingElements(arr1, arr2){
+function countMatchingElements(array1, array2){
     const intersection = array1.filter(element => array2.includes(element));
     var count = intersection.length;
     return count;
 }
 
-function createNewPhase(names){
-    if (!names){
-        
+function f2(count, sign, margin){
+    if(sign == "ge"){
+        if(count >= margin){
+            return true;
+        } else {
+            return false;
+        }
+    } else if (sign == "le"){
+        if(count <= margin){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    else {
+        return false;
     }
 }
 
+function f3(arr, sign, margin, data){
+    var result = [];
+    data.forEach(e => {
+        var count = countMatchingElements(e.numList, arr);
+        var compare = f2(count, sign, margin);
+        if (compare){
+            result.push({
+                name: e.name,
+                numList: e.numList,
+                value: count
+            })
+        }
+    })
+    return result;
+}
+
+async function f4a(arr, sign, margin){
+    var result = [];
+    await NumList.find()
+        .then((data) => {
+            result = f3(arr, sign, margin, data);
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+    return result;
+}
+
+
+async function f4b(arr, phaseCode, sign, margin){
+    var result = [];
+    await CompareList.find({phaseCode: phaseCode})
+        .then((data) => {
+            var len = data.length;
+            if (len == 0) {
+                result = f4a(arr, sign, margin);
+            } else {
+                var lastStep = getLastStep(data);
+                result = f3(arr, sign, margin, lastStep);
+            }            
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+    return result;
+}
+
+
 //controller
-function createCompareListFromInputAndName(arr, phaseCode, sign, origin){
+// function createCompareListFromInputAndName(arr, phaseCode, sign, margin){
     
+//     if(!phaseCode){
+//         createNewPhase();
+//     }
+//     else{
+//         names = getNames(phaseCode);
+//         createNewPhase(names);
+//     }
+// }
+
+function createCompareList(arr, phaseCode, sign, margin){
     if(!phaseCode){
-        createNewPhase();
+        var CompareListData = await f4a(arr,sign,margin);
+        var compareList = new CompareList({
+            phaseCode: phaseCode,
+            phaseStep: 
+        })
     }
     else{
-        names = getNames(phaseCode);
-        createNewPhase(names);
+        var CompareListData = await f4b(arr, phaseCode, sign, margin);
     }
 }
